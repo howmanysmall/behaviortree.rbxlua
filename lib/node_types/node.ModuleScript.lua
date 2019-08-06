aaa571cd1befe64997cc2be0e863d945f6ae5638
howmanysmall <lhamil29@bruinmail.slcc.edu>
2019-08-05 19:21:30 -0600

    Update node.ModuleScript.lua

--- lib/node_types/node.ModuleScript.lua
+++ lib/node_types/node.ModuleScript.lua
@@ -2,6 +2,8 @@ local class         = require(game.ReplicatedStorage.BehaviorTree.middleclass)
 local Registry      = require(game.ReplicatedStorage.BehaviorTree.registry)
 local Node          = class('Node')
 
+local pairs = pairs -- slow right now, fast in the future
+
 function Node:initialize(config)
   config = config or {}
   for k, v in pairs(config) do
@@ -18,9 +20,9 @@ function Node:finish() end
 function Node:run() end
 
 function Node:call_run(object)
-  success = function() self:success() end
-  fail = function()    self:fail() end
-  running = function() self:running() end
+  local success = function() self:success() end -- what are this and why do are it exist
+  local fail = function()    self:fail() end
+  local running = function() self:running() end
   self:run(object)
   success, fail, running = nil,nil,nil
 end
